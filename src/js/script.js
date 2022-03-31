ffa0c0ef9d76ed802116af1c5937c5ad39a39d5b
Hose9817 <abdullayev6859@gmail.com>
2022-03-31 09:28:12 +0600

    added modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -23,9 +23,9 @@ $(document).ready(function () {
             .index()).addClass('catalog__content_active');
     });
 
-    function toggleSlide(item){
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -35,5 +35,22 @@ $(document).ready(function () {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn();
+    });
+    
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut(1000);
+    })
+
+    $('.button_mini').each(function (i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    })
 
 });
