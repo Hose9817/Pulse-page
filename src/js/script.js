5cbcee1c322b4a895fc3a9bdc63c6a89fc625e31
Hose9817 <abdullayev6859@gmail.com>
2022-04-02 01:31:03 +0600

    added validation for all forms

--- src/js/script.js
+++ src/js/script.js
@@ -41,20 +41,84 @@ $(document).ready(function () {
     $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn();
     });
-    
-    $('.modal__close').on('click', function() {
+
+    $('.modal__close').on('click', function () {
         $('.overlay, #consultation, #order, #thanks').fadeOut();
     })
 
-    $('.button_mini').each(function (i){
-        $(this).on('click', function(){
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn();
         })
     })
 
-    $('#consultation-form').validate();
-    $('#consultation form').validate();
-    $('#order form').validate();
+    //Validation of forms
+
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: 'required',
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Input your name, please",
+    //             minlength: jQuery.validator.format("Please type {0} characters!")
+    //         },
+    //         phone: "Input yur phone number",
+    //         email: {
+    //             required: "We need your email",
+    //             email: "Your email have not correct format"
+    //         }
+    //     }
+    // });
+    // $('#order form').validate({
+    //     rules: {
+    //         name: 'required',
+    //         phone: 'required',
+    //         email: 'required'
+    //     }
+    // });
+
+    function validForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Input your name, please",
+                    minlength: jQuery.validator.format("Please type {0} characters!")
+                },
+                phone: "Input yur phone number",
+                email: {
+                    required: "We need your email",
+                    email: "Your email have not correct format"
+                }
+            }
+        });
+    }
+
+    validForms('#consultation-form');
+
+    validForms('#consultation form');
+
+    validForms('#order form');
 
 });
